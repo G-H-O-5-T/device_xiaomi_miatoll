0950ec47ff20c33afa5f81019919f8dcab22ca80
G-H-O-5-T <jijinraj401@gmail.com>
2021-04-05 14:19:32 +0000

    * bringup with los variables
    Change-Id: Idca1dc7212ff752c8a52cdc95ab7f9b3e0ffee11

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := fluid_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -28,7 +28,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 IS_PHONE := true
 DISABLE_DEFAULT_CAMERA := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-FLUID_BUILD_TYPE := OFFICIAL
-PRODUCT_PRODUCT_PROPERTIES += \
-  ro.fluid.maintainer=Ramisky \
-  ro.fluid.cpu=SDM720G
